3998f5b6262a4c299aa81de6e881b2280d41948f
Jack <yh.io@outlook.com>
2024-08-12 15:20:03 +0800

    log remote addr

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -13,7 +13,7 @@ var log = logging.Logger("middleware")
 
 func Timer(handler http.HandlerFunc, name string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Debugw("request", "method", r.Method, "path", r.URL.Path, "name", name)
+		log.Debugw("request", "method", r.Method, "path", r.URL.Path, "name", name, "remoteAddr", r.RemoteAddr)
 
 		ctx, _ := tag.New(context.Background(), tag.Upsert(metrics.Endpoint, name))
 		stop := metrics.Timer(ctx, metrics.APIRequestDuration)
